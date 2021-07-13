ce19fd9770a89230c8bdb453cffb0ef66ffaf3e6
Daniel Kerry <psxdk7@exmail.nottingham.ac.uk>
2021-07-13 09:00:23 +0100

    Funcitoning seller price modification

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ listing = []
 orders = []
 deliveries = []
 
-
 def getItem():
     return items[random.randint(0, len(items))]
 
@@ -49,12 +48,9 @@ class Buyer(Agent):
     def __init__(self, unique_id):
         self.unique_id = unique_id
         self.wealth = int(random.triangular(5, 10, 15))
-        self.desires = getList(int(random.triangular(1, 3, 5)))
+        self.desires = getList(int(random.triangular(0, 1, 2)))
         self.inventory = []
         self.MSRPs = []
-        for x in range(len(self.desires)):
-            self.MSRPs.append(prices[self.desires[x]])
-
         self.orders = []
         self.patience = 0
 
@@ -67,66 +63,75 @@ class Buyer(Agent):
         CheapFirstPrice = 100
         CheapFirstSeller = 0
 
-        for x in range(len(self.desires)):
-            for y in range(len(listing)):
-                #Selects item with greatest percentage discount
-                if self.desires[x] in listing[y][0]:
-                    ind = listing[y][0].index(self.desires[x])
-                    sellerPrice = listing[y][1][ind]
-                    discount = sellerPrice / self.MSRPs[x]
-                    if discount < MaxPercent:
-                        MaxPercent = discount
-                        MaxPercentPrice = sellerPrice
-                        MaxPercentInd = listing[y][2]
-                        MaxPercentItem = self.desires[x]
-                    elif discount == MaxPercent:
-                        if sellerPrice > MaxPercentPrice:
+        self.MSRPs = []
+        if len(self.desires) > 0:
+            for x in range(len(self.desires)):
+                self.MSRPs.append(prices[self.desires[x]])
+
+            for x in range(len(self.desires)):
+                for y in range(len(listing)):
+                    #Selects item with greatest percentage discount
+                    if self.desires[x] in listing[y][0]:
+                        ind = listing[y][0].index(self.desires[x])
+                        sellerPrice = listing[y][1][ind]
+                        discount = sellerPrice / self.MSRPs[x]
+                        if discount < MaxPercent:
                             MaxPercent = discount
                             MaxPercentPrice = sellerPrice
                             MaxPercentInd = listing[y][2]
                             MaxPercentItem = self.desires[x]
+                        elif discount == MaxPercent:
+                            if sellerPrice > MaxPercentPrice:
+                                MaxPercent = discount
+                                MaxPercentPrice = sellerPrice
+                                MaxPercentInd = listing[y][2]
+                                MaxPercentItem = self.desires[x]
 
-                if self.desires[0] in listing[y][0]:
-                    ind = listing[y][0].index(self.desires[0])
-                    price = listing[y][1][ind]
-                    if price < CheapFirstPrice:
-                        CheapFirstPrice = price
-                        CheapFirstSeller = listing[y][2]
+                    if self.desires[0] in listing[y][0]:
+                        ind = listing[y][0].index(self.desires[0])
+                        price = listing[y][1][ind]
+                        if price < CheapFirstPrice:
+                            CheapFirstPrice = price
+                            CheapFirstSeller = listing[y][2]
 
-        MaxPercentResult = orderSheet(self.unique_id, MaxPercentInd, MaxPercentPrice, MaxPercentItem)
-        FirstListedItem = orderSheet(self.unique_id, CheapFirstSeller, CheapFirstPrice, self.desires[0])
+            MaxPercentResult = orderSheet(self.unique_id, MaxPercentInd, MaxPercentPrice, MaxPercentItem)
+            FirstListedItem = orderSheet(self.unique_id, CheapFirstSeller, CheapFirstPrice, self.desires[0])
+            tempIndicator = random.randint(0, 1)
 
-        tempIndicator = random.randint(0, 1)
-
-        if tempIndicator == 0:
-            return MaxPercentResult
-        elif tempIndicator == 1:
-            return FirstListedItem
-        return provisionalOrder
+            if tempIndicator == 0:
+                return MaxPercentResult
+            elif tempIndicator == 1:
+                return FirstListedItem
+            return provisionalOrder
+        else:
+            provisionalOrder = None
+            return provisionalOrder
 
     def makePurchase(self):
         order = self.evaluateItems()
-        if order == None:
-            pass
-        if self.wealth > order.price:
-            self.wealth -= order.price
-            order.ExpectedArrivalDate, order.SaleDate = Test.epoch + 5, Test.epoch
-            orders.append(order)
-            '''NEEDS BETTER CALCULATION BASED ON PARAMETERS'''
-            #dueDate = Test.epoch + 5
-            #incomingOrder = orderTrack(self.unique_id, order.SellerID, Test.epoch, dueDate)
-            #self.orders.append(incomingOrder)
-            self.desires[self.desires.index(order.item)] = None
+        if order != None:
+            if self.wealth > order.price:
+                self.wealth -= order.price
+                #TODO: Expectations are always the same at the moment for buyers
+                order.ExpectedArrivalDate, order.SaleDate = Test.epoch + 5, Test.epoch
+                orders.append(order)
+                '''NEEDS BETTER CALCULATION BASED ON PARAMETERS'''
+                #dueDate = Test.epoch + 5
+                #incomingOrder = orderTrack(self.unique_id, order.SellerID, Test.epoch, dueDate)
+                #self.orders.append(incomingOrder)
+                self.desires[self.desires.index(order.item)] = None
 
-        delList = []
-        for z in range(len(self.desires)):
-            if self.desires[z] == None:
-                delList.append(z)
-        if len(delList) < 2:
-            for a in range(len(delList)):
-                del(self.desires[delList[a]])
+            delList = []
+            for z in range(len(self.desires)):
+                if self.desires[z] == None:
+                    delList.append(z)
+            if len(delList) < 2:
+                for a in range(len(delList)):
+                    del(self.desires[delList[a]])
+            else:
+                self.desires = []
         else:
-            self.desires = []
+            pass
 
     def evaluateOrders(self):
         for i in range(len(orders)):
@@ -136,15 +141,19 @@ class Buyer(Agent):
                         Test.feedback[orders[i].SellerID].append(False)
                     elif orders[i].ActualArrivalDate <= orders[i].ExpectedArrivalDate:
                         Test.feedback[orders[i].SellerID].append(True)
+                        self.inventory.append(orders[i].item)
 
     def leaveFeedback(self):
         pass
 
     def newItems(self):
         if self.desires == []:
-            self.desires.append(getItem())
+            #TODO: Find a better way to assign new items
+            if random.randint(0,2) == 1:
+                self.desires.append(getItem())
 
     def makeMoney(self):
+        #TODO: Increase complexity of wealth increase
         earnings = int(random.triangular(2, 3, 4))
         self.wealth += earnings
 
@@ -153,17 +162,20 @@ class Buyer(Agent):
         self.evaluateOrders()
         self.newItems()
         self.makeMoney()
-        print(f"BuyerID: {self.unique_id}, Desires: {self.desires}, Inventory: {self.inventory}, Wealth: {self.wealth}")
+        #print(f"BuyerID: {self.unique_id}, Desires: {self.desires}, Inventory: {self.inventory}, Wealth: {self.wealth}")
 
 class Seller(Agent):
     def __init__(self, unique_id):
         self.unique_id = unique_id
         self.inventory = getList(int(random.triangular(2, 3, 4)))
         self.prices = []
-        self.pendingOrders = []
+        self.tracking = []
+        for i in range(len(self.inventory)):
+            self.tracking.append([0,0])
+
         self.accuracy = int(random.triangular(1, 35, 50))
         self.speed = int(random.triangular(2, 5, 8))
-        '''Assigns Sellers with varied prices'''
+
         for x in range(len(self.inventory)):
             temp = prices[self.inventory[x]]
             temp = int(random.triangular(temp-2, temp, temp+2))
@@ -173,6 +185,7 @@ class Seller(Agent):
 
     def generateDeliveryTime(self, date):
         test_digit = random.randint(0, self.accuracy)
+        #TODO: Make delivery time rely more heavily on parameters
         if test_digit < 3:
             return (date + self.speed + 5)
         else:
@@ -185,23 +198,50 @@ class Seller(Agent):
             if orders[y].SellerID == self.unique_id and orders[y].ActualArrivalDate == None :
                 deliveryDate = self.generateDeliveryTime(orders[y].SaleDate)
                 orders[y].ActualArrivalDate = deliveryDate
+                x = self.inventory.index(orders[y].item)
+                self.tracking[x][0] += 1
+                self.tracking[x][1] = 0
 
-        for i in range(len(self.pendingOrders)):
-            print(self.pendingOrders[i])
+    def updatePrices(self):
+        saleLimit = 50
+        reductionLimit = 25
+        """If Item is sold 50+ times, it is removed from stock and replaced by a new item"""
+        """If an item goes 25 iterations without a sale, its price is reduced by 1"""
+        for i in range(len(self.tracking)):
+            self.tracking[i][1] += 1
+            if self.tracking[i][0] > saleLimit:
+                while True:
+                    newitem = getItem()
+                    if newitem in self.inventory:
+                        continue
+                    else:
+                        break
+                self.inventory[i] = newitem
+                self.tracking[i] = [0, 0]
+                #Setting new prices for regenerated items
+                newPrice = int(random.triangular(prices[newitem]-2, prices[newitem], prices[newitem]+2))
+                self.prices[i] = newPrice
+            if self.tracking[i][1] > reductionLimit:
+                self.prices[i] -= 1
+                self.tracking[i][1] = 0
 
+    def __str__(self):
+        return f"Inventory: {self.inventory}, Prices: {self.prices}, Tracking: {self.tracking}"
 
     def step(self):
         self.orderCheck()
-
+        self.updatePrices()
+        if self.unique_id == 3:
+            print(self)
 
 class Market(Model):
-    def __init__(self, B = 10, S = 5):
+    def __init__(self, B = 100, S = 35):
         self.numOfBuyers = B
         self.numOfSellers = S
         self.schedule = RandomActivation(self)
         self.epoch = 0
         self.feedback = []
-        for x in range(300):
+        for x in range(1000):
             self.feedback.append([])
 
         for i in range(1, self.numOfSellers * 2, 2):
@@ -228,8 +268,7 @@ class Market(Model):
     def step(self):
         self.schedule.step()
         self.epoch += 1
-        print(listing)
-
+        #print(listing)
         print(self.epoch)
         self.calculateSellerTrust()
 
